37dc811bb1895d93466a8118f717be979317a9d0
Aliaksiej Artamonaŭ <aliaksiej.artamonau@gmail.com>
2010-11-18 11:20:55 +0200

    Monadic maps over graphs.

--- src/AI/NEAT/Utils/Graph.hs
+++ src/AI/NEAT/Utils/Graph.hs
@@ -17,12 +17,27 @@
 ------------------------------------------------------------------------------
 module AI.NEAT.Utils.Graph
        (
-         modifyEdges
+         -- * Miscellaneous
+         modifyEdges,
+
+         -- * Monadic computations
+         --
+         -- ** Sequencing monadic computations
+         --
+         --    $sequencingNote
+         sequenceGrN,
+         sequenceGrE,
+         sequenceGr,
+
+         -- ** Monadic maps
+         nmapM,
+         emapM
        ) where
 
 
 ------------------------------------------------------------------------------
-import Data.Graph.Inductive ( DynGraph, Node, (&), match, out' )
+import Data.Graph.Inductive ( DynGraph, Node, (&),
+                              match, out', nmap, emap, ufold, empty )
 
 
 ------------------------------------------------------------------------------
@@ -41,3 +56,66 @@ modifyEdges (src, dst) f g = maybe g (& g') newContext
                                                        then (f l, n)
                                                        else (  l, n))
                                            os)
+
+
+------------------------------------------------------------------------------
+-- $sequencingNote
+-- There is no well defined order in which sequencing of monadic actions is
+-- performed by the following functions. So those computations must not depend
+-- on the order.
+------------------------------------------------------------------------------
+-- | Evaluates monadic computations stored in nodes' labels.
+sequenceGrN :: (Monad m, DynGraph gr)
+            => gr (m a) b
+            -> m (gr a b)
+sequenceGrN = ufold k (return empty)
+  where k (ins, n, l, outs) rest = do
+          l'    <- l
+          rest' <- rest
+
+          return $ (ins, n, l', outs) & rest'
+
+
+------------------------------------------------------------------------------
+-- | Evaluates monadic computations stored in edges' labels.
+sequenceGrE :: (Monad m, DynGraph gr)
+            => gr a (m b)
+            -> m (gr a b)
+sequenceGrE = ufold k (return empty)
+  where k (ins, n, l, outs) rest = do
+          ins'  <- seqAdjs ins
+          outs' <- seqAdjs outs
+          rest' <- rest
+
+          return $ (ins', n, l, outs') & rest'
+
+        seqAdjs xs = do
+          let ns = map snd xs
+          ls    <- sequence (map fst xs)
+
+          return $ zip ls ns
+
+------------------------------------------------------------------------------
+-- | Evalues monadic computations from both nodes' and edges' labels.
+sequenceGr :: (Monad m, DynGraph gr)
+           => gr (m a) (m b)
+           -> m (gr a b)
+sequenceGr g = sequenceGrN g >>= sequenceGrE
+
+
+------------------------------------------------------------------------------
+-- | Maps monadic action over the labels of graph nodes.
+nmapM :: (Monad m, DynGraph gr)
+      => (a -> m c)
+      -> gr a b
+      -> m (gr c b)
+nmapM k g = sequenceGrN (nmap k g)
+
+
+------------------------------------------------------------------------------
+-- | Maps monadic action over the labels of graph edges.
+emapM :: (Monad m, DynGraph gr)
+      => (b -> m c)
+      -> gr a b
+      -> m (gr a c)
+emapM k g = sequenceGrE (emap k g)

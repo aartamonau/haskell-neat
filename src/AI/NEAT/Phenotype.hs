e2e6fa5795ee1be291d6e44b9e86cf96db63df53
Aliaksiej Artamonaŭ <aliaksiej.artamonau@gmail.com>
2010-11-24 16:52:41 +0200

    Phenotype interface sketch.

---
+++ src/AI/NEAT/Phenotype.hs
@@ -0,0 +1,49 @@
+------------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------------
+module AI.NEAT.Phenotype
+       (
+         NeuralNet
+       ) where
+
+
+------------------------------------------------------------------------------
+import Data.Graph.Inductive.PatriciaTree ( Gr )
+
+import AI.NEAT.Genome           ( Genome )
+import AI.NEAT.Genome.Neuron    ( NeuronGene )
+import AI.NEAT.Genome.Link      ( LinkGene )
+import AI.NEAT.Phenotype.Neuron ( Neuron )
+import AI.NEAT.Phenotype.Link   ( Link )
+
+
+------------------------------------------------------------------------------
+data NeuralNet =
+  NeuralNet { inputs  :: !Int
+            , outputs :: !Int
+            , net     :: !(Gr Neuron Link)
+            }
+
+
+------------------------------------------------------------------------------
+-- | Recomputes outputs of all the neurons according to new input.
+update :: NeuralNet -> [Double] -> NeuralNet
+update = undefined
+
+
+------------------------------------------------------------------------------
+-- | Returns current output of neural net.
+getOutputs :: NeuralNet -> [Double]
+getOutputs = undefined
+
+
+------------------------------------------------------------------------------
+-- | Builds neural net from genome.
+fromGenome :: Genome -> NeuralNet
+fromGenome = undefined
+  where neuronFromGene :: NeuronGene -> Neuron
+        neuronFromGene = undefined
+
+        linkFromGene :: LinkGene -> Link
+        linkFromGene = undefined
